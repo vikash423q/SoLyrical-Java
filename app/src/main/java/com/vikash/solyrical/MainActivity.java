143894a75af186ee215fb4328c4268e446418649
vikashkumar <vikash423q@gmail.com>
2018-08-03 19:42:31 +0530

    second commit

--- app/src/main/java/com/vikash/solyrical/MainActivity.java
+++ app/src/main/java/com/vikash/solyrical/MainActivity.java
@@ -1,11 +1,25 @@
 package com.vikash.solyrical;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,9 +29,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    ArrayList<String> arrayList=new ArrayList<String>();
+    ArrayAdapter<String> arrayAdapter;
+    SQLiteDatabase database;
+    MediaPlayer mediaPlayer=new MediaPlayer();
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +56,26 @@ public class MainActivity extends AppCompatActivity
         FragmentManager fragmentManager=getSupportFragmentManager();
         FragmentTransaction ft=fragmentManager.beginTransaction();
         ft.add(R.id.fragment,fragment);
-        ft.commit();
 
 
+        ListView listView=(ListView)findViewById(R.id.listView);
+
+        arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Logic on item click.
+                if(mediaPlayer.isPlaying())
+                    mediaPlayer.stop();
+                Uri uri= Uri.parse(SongFinder.hashMap.get(arrayList.get(position)));
+                Log.i("Uri",uri.toString());
+                mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
+                mediaPlayer.start();
+            }
+        });
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -41,7 +84,54 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        SongRetriever retriever=new SongRetriever();
+        retriever.execute("");
     }
+
+    public void getFromStorage(){
+
+        database=this.openOrCreateDatabase("Songs",MODE_PRIVATE,null);
+        database.execSQL("CREATE TABLE IF NOT EXISTS songs(id INTEGER primary key autoincrement,title VARCHAR,filepath VARCHAR)");
+        Cursor c=database.rawQuery("SELECT * FROM songs",null);
+
+        if(c.moveToNext()){
+            //data exists.
+
+            do{
+
+            }while(c.moveToNext());
+        }
+        else{
+            //data doesn't exists.Create one.
+            getFromLocal();
+        }
+
+        return;
+    }
+
+    public void getFromLocal(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            return;
+        }
+        SongFinder songFinder=new SongFinder();
+        songFinder.find(arrayList);
+        Log.i("song","List being updated");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                getFromLocal();
+        }
+    }
+
+    public void mediaControl(){
+
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -98,5 +188,21 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public class SongRetriever extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            getFromStorage();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            arrayAdapter.notifyDataSetChanged();
+
+        }
     }
 }
---
+++ app/src/main/java/com/vikash/solyrical/SongFinder.java
@@ -0,0 +1,47 @@
+package com.vikash.solyrical;
+
+import android.Manifest;
+import android.content.Context;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SongFinder {
+
+    File file= Environment.getExternalStorageDirectory();
+    String root=file.getPath()+"/";
+    ArrayList<String> songs=new ArrayList<String>();
+    static HashMap<String,String> hashMap=new HashMap<String, String>();
+
+    public ArrayList<String> find(ArrayList<String> list){
+
+        songs=list;
+        File[] files=file.listFiles();
+        scanDirectory(files);
+
+        return list;
+    }
+
+    public void scanDirectory(File[] files){
+        for(File temp:files){
+            if(temp.isDirectory()&&!temp.toString().contains("/."))
+                scanDirectory(temp.listFiles());
+            else
+                filterSong(temp);
+        }
+    }
+
+    public void filterSong(File file){
+        String name="";
+        if(file.getAbsolutePath().endsWith(".mp3")){
+            name=file.getName().substring(0,file.getName().length()-4);
+            songs.add(name);
+            hashMap.put(name,file.getAbsolutePath());
+        }
+    }
+}
